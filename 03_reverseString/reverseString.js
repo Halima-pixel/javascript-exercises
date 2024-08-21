06ca583f187c06d30c95140c19bc3f688fd5b1da
halima-pixel <sannianuoluwapo610@gmail.com>
2024-08-21 21:49:25 +0100

    Passes The test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
+const reverseString = function reverseString(str) {
+    return str.split('').reverse('').join('');
 
 };
+ 
+reverseString('Hello');
 
 // Do not edit below this line
 module.exports = reverseString;
