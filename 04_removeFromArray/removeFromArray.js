cadeea874f9d9e981a4196e4201483a5480dff08
halima-pixel <sannianuoluwapo610@gmail.com>
2024-08-22 00:16:57 +0100

    Test Passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function removeFromArray(ary, val) {
+     let index  = ary.indexOf(val);
+    if (index !== -1) {
+        ary.splice(index, 1);
+    }
+    return ary;    
 };
+    let array = [ 1, 2, 3, 4];
+     removeFromArray(array, 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
+console.log(removeFromArray);
