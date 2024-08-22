060dbf982650dfe3c5e9b77619def0b14a349bc9
halima-pixel <sannianuoluwapo610@gmail.com>
2024-08-22 08:13:46 +0100

    "Implemented sumAll function to calculate sum of numbers between two given numbers"

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
 
-};
+const sumAll = function sumAll(a, b) {
+    let sum = 0; // create a variable to hold the final sum
+    for (let i = Math.min(a, b); i <= Math.max(a, b); i++) { // loop through the given numbers
+      sum += i; // on each iteration add the number to the sum
+    }
+    return sum; // return the sum after finishing the loop
+  };
+  sumAll(1, 4);
 
-// Do not edit below this line
-module.exports = sumAll;
+  // Do not edit below this line
+  module.exports = sumAll;
+
+
+
