befa4f4744cde318a0423359d13bf284437eb835
halima-pixel <sannianuoluwapo610@gmail.com>
2024-08-22 12:02:51 +0100

    "implemented temperature conversion functions in tempConversion.js"

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,10 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = (fahrenheit) => Math.round(((fahrenheit - 32 ) * (5 / 9)));
+const convertToFahrenheit =  (celsius) => Math.round(((celsius *  (9 / 5)) + 32));
 
-const convertToFahrenheit = function() {
-};
+console.log(convertToCelsius(32)); // Output: 0
+console.log(convertToFahrenheit(0)); // Output: 32
 
-// Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
